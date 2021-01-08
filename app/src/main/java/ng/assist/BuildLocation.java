40336ece4188d24ce3af7a2f1c10d51a06ce3ddb
darmilola <damilolaakinterinwa@gmail.com>
2021-01-08 10:26:07 +0100

    Transport Progress

--- app/src/main/java/ng/assist/BuildLocation.java
+++ app/src/main/java/ng/assist/BuildLocation.java
@@ -3,17 +3,29 @@ package ng.assist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.google.android.material.button.MaterialButton;
+
 public class BuildLocation extends AppCompatActivity {
 
+    MaterialButton next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_build_location);
+        next = findViewById(R.id.build_root_next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BuildLocation.this,BusTransport.class));
+            }
+        });
     }
 
     @Override
