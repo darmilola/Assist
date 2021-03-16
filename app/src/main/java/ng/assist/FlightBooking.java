db17f7b986d7c280bfc3c96724800cb86ef9f8d4
darmilola <damilolaakinterinwa@gmail.com>
2021-03-16 14:55:21 +0100

    General Progress

---
+++ app/src/main/java/ng/assist/FlightBooking.java
@@ -0,0 +1,15 @@
+package ng.assist;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FlightBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_booking);
+    }
+}
